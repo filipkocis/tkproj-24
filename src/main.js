54e80d1b8ce65c2bef7e2be19bebb4b295f45513
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-12-31 13:52:30 +0100

    feat(main): implement view loading

--- src/main.js
+++ src/main.js
@@ -1,22 +1,80 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 class="border border-black p-4">Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-    <div id="daterange">counter</div>>
-  </div>
-`
+import { availabilities } from './availabilities.js'
+import { listComponent, overlayComponent, topComponent } from './components.js';
+import { setupPicker } from './picker.js'
+
+function loadFirstView() {
+  // insert components
+  const app = document.querySelector('#app');   
+  app.insertAdjacentHTML('beforeend', topComponent())
+  app.insertAdjacentHTML('beforeend', listComponent())
+  app.insertAdjacentHTML('beforeend', overlayComponent())
+
+  // center picker
+  window.addEventListener('resize', centerPicker)
+  centerPicker()
+
+  // event listener for first selection
+  const picker = setupPicker('#picker')
+
+  // add event listeners
+  addPickerListeners(picker)
+
+  const listener = (_) => {
+    const overlay = document.querySelector('#overlay')
+    const container = document.querySelector('#picker-container')
+
+    overlay.style.opacity = 0
+    container.style.transitionDuration = '1s';
+    container.style.transform = ""
+    container.style.width = '180px'
+    container.style.height = '50px'
+    container.style.fontSize = '16px'
+
+    picker.off("select", listener)
+    window.removeEventListener('resize', centerPicker)
+  };
+  picker.on("select", listener)
+}
+loadFirstView()
+
+/**
+ * Center the picker container to the middle of the screen
+ */
+function centerPicker() {
+  const container = document.querySelector('#picker-container')
+  container.style.transform = ''
+
+  const shw = window.innerWidth / 2
+  const shh = window.innerHeight / 2
+  const rect = container.getBoundingClientRect();
+
+  const translate = {
+    x: (shw - rect.width / 2) - rect.x,
+    y: (shh - rect.height / 2) - rect.y,
+  };
+
+  container.style.transform = `translate(${translate.x}px, ${translate.y}px)`
+  container.style.zIndex = 10;
+}
+
+function addPickerListeners(picker) {
+  const button = document.querySelector('#picker-button')
+
+  const firstClickHandler = async () => {
+    await availabilities.load()
+
+    picker.renderAll()
+    clickHandler()
+
+    button.addEventListener('click', clickHandler)
+    button.removeEventListener('click', firstClickHandler)
+  }
+
+  const clickHandler = () => {
+    document.querySelector('#picker').click()
+  }
+
+  button.addEventListener('click', firstClickHandler)
+}
